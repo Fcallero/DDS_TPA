fa0b568f28ef3dafe50489bd1283ecc422199619
Franco Callero <fcallero@frba.utn.edu.ar>
2024-12-13 12:53:16 -0300

    agrego error de seguridad importante

--- src/main/java/org/example/Application.java
+++ src/main/java/org/example/Application.java
@@ -37,7 +37,7 @@ public class Application {
                             cors ->
                             cors.add(CorsPluginConfig::anyHost)); // para poder hacer requests de un dominio a otro
 
-                    javalinConfig.staticFiles.add("/public", Location.CLASSPATH); // recursos estaticos (HTML, CSS, JS, IMG)
+                    javalinConfig.staticFiles.add("", Location.CLASSPATH); // recursos estaticos (HTML, CSS, JS, IMG)
                     javalinConfig.routing.contextPath = "";
 
                     javalinConfig.jetty.server(() -> {
